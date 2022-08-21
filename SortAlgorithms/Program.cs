89f2401e81b1e032ffefb72c9dc8c102db6c575b
1mosce <shuliaknikolay@gmail.com>
2022-08-21 20:55:30 +0300

    Init

--- SortAlgorithms/Program.cs
+++ SortAlgorithms/Program.cs
@@ -1,16 +1,46 @@
 ﻿public class SortAlgorithms
 {
-    public static void Main(string[] args)
+    //метод обмена элементов
+    static void Swap(ref int e1, ref int e2)
     {
+        var temp = e1;
+        e1 = e2;
+        e2 = temp;
+    }
 
-        int[] array = { 1, 2, 3, 4, 5, 6 };
-        Console.WriteLine("Given array length " + array.Length);
-        Console.WriteLine("Array content");
-        for (int i = 0; i < array.Length; i++)
+    //сортировка пузырьком
+    static int[] BubbleSort(int[] array)
+    {
+        var len = array.Length;
+        for (var i = 1; i < len; i++)
         {
-            Console.Write(+ array[i] + " , ");
+            for (var j = 0; j < len - i; j++)
+            {
+                if (array[j] > array[j + 1])
+                {
+                    Swap(ref array[j], ref array[j + 1]);
+                }
+            }
         }
 
-       
+        return array;
     }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Сортировка пузырьком");
+        Console.Write("Введите элементы массива: ");
+        var parts = Console.ReadLine().Split(new[] { " ", ",", ";" }, StringSplitOptions.RemoveEmptyEntries);
+        var array = new int[parts.Length];
+        for (int i = 0; i < parts.Length; i++)
+        {
+            array[i] = Convert.ToInt32(parts[i]);
+        }
+
+        Console.WriteLine("Отсортированный массив: {0}", string.Join(", ", BubbleSort(array)));
+
+        Console.ReadLine();
+    }
+
 }
+
