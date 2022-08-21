840522162735f8a9870aa61761aa7f2d238fbe8d
1mosce <shuliaknikolay@gmail.com>
2022-08-21 20:30:43 +0300

    Add project files.

---
+++ SortAlgorithms/Program.cs
@@ -0,0 +1,16 @@
+﻿public class SortAlgorithms
+{
+    public static void Main(string[] args)
+    {
+
+        int[] array = { 1, 2, 3, 4, 5, 6 };
+        Console.WriteLine("Given array length " + array.Length);
+        Console.WriteLine("Array content");
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(+ array[i] + " , ");
+        }
+
+       
+    }
+}
